44781db7933f045038caf83e08e6508b323bbf2f
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-06 00:32:57 +0600

    Number Mirror

---
+++ START01.cpp
@@ -0,0 +1,13 @@
+//Problem NO: START01
+//https://www.codechef.com/problems/START01
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+
+    cin>> n;
+    cout<< n << endl;
+
+    return 0;
+}
