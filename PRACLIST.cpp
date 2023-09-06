56699f207baf093e52df9d31fdab7ae92f4e0fe3
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-07 00:49:03 +0600

    How many unattempted problems

---
+++ PRACLIST.cpp
@@ -0,0 +1,16 @@
+//Problem no: PRACLIST
+//https://www.codechef.com/problems/PRACLIST
+
+#include<iostream>
+using namespace std;
+int main(){
+    int X,Y,dif;
+
+    cin >> X >> Y;
+
+    dif=X-Y;
+
+    cout << dif << endl;
+
+    return 0;
+}
