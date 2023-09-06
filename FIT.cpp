b2e9cbdeda0c4c59728547a1ae55e5d3dee357bd
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-07 00:45:22 +0600

    Fitness

---
+++ FIT.cpp
@@ -0,0 +1,17 @@
+//Problem name: FIT
+//https://www.codechef.com/problems/FIT
+
+#include<iostream>
+using namespace std;
+int main(){
+    int T,X,i;
+
+    cin >> T;
+    
+    for(i=0; i<T; i++){
+        cin >> X;
+        cout << X*10 <<endl;
+    }
+
+    return 0;
+}
