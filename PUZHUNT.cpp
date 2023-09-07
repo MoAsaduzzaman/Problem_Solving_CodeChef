adc302d420f4e547b1cb83b117b05760a881f586
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-07 11:31:56 +0600

    Puzzle Hunt

---
+++ PUZHUNT.cpp
@@ -0,0 +1,18 @@
+//Problem name: PUZHUNT
+//https://www.codechef.com/problems/PUZHUNT
+
+#include<iostream>
+using namespace std;
+int main(){
+    int N;
+
+    cin >> N;
+
+    if(N==6 || N==7 || N==8){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
