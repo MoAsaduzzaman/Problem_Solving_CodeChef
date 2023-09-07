5374bc9adb3f788d0f8ef57a12c199ab9e4c48a6
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-07 12:13:22 +0600

    2000

---
+++ RIP2000.cpp
@@ -0,0 +1,16 @@
+//Problem name: RIP2000
+//https://www.codechef.com/problems/RIP2000
+
+#include<iostream>
+using namespace std;
+int main(){
+    int N,ans;
+
+    cin >> N;
+
+    ans = (N*2000)/500;
+
+    cout << ans << endl;
+    
+    return 0;
+}
