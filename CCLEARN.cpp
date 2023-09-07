29d3419ea9f7366d56f118d65347651aa79c0777
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-07 12:27:45 +0600

    CodeChef Learn Problem Solving

---
+++ CCLEARN.cpp
@@ -0,0 +1,14 @@
+//Problem name: CCLEARN
+//https://www.codechef.com/problems/CCLEARN
+
+#include<iostream>
+using namespace std;
+int main(){
+    int N;
+
+    cin >> N;
+
+    cout << N*2 << endl;
+
+    return 0;
+}
