d32ca664f60486470384d21ff9dbfa9e57835c39
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-09 00:11:54 +0600

    Email Reminders

---
+++ EMAILREM.cpp
@@ -0,0 +1,15 @@
+//Problem name: EMAILREM
+//https://www.codechef.com/problems/EMAILREM
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int N,U;
+
+    cin >> N >> U;
+
+    cout << N-U << endl;
+ 
+    return 0;
+}
