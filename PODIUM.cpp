8d4c76f079ad889a7ec856fb0eab8208be943d31
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-08 19:22:50 +0600

    Podium Finish

---
+++ PODIUM.cpp
@@ -0,0 +1,17 @@
+//Problem name: PODIUM
+//https://www.codechef.com/problems/PODIUM
+
+#include<iostream>
+using namespace std;
+int main(){
+    int T,A,B,i;
+
+    cin >> T;
+
+    for(i=0; i<T; i++){
+        cin >> A >> B;
+        cout << A+B << endl;
+    }
+
+    return 0;
+}
