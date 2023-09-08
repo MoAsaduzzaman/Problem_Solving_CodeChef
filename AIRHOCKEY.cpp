399e59ce6b1c95188f8a2f4b7556f057a78e3752
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-09 00:18:26 +0600

    Air Hockey

---
+++ AIRHOCKEY.cpp
@@ -0,0 +1,25 @@
+//Problem name: AIRHOCKEY
+//https://www.codechef.com/problems/AIRHOCKEY
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int T,A,B;
+
+    cin >> T;
+
+    while(T--){
+
+        cin >> A >> B;
+
+        if(A>=B){
+            cout << 7-A << endl;
+        }
+        if(A<B){
+            cout << 7-B << endl;
+        }
+    }
+
+    return 0;
+}
