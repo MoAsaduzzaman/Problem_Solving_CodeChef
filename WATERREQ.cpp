c9f6828b2f370afbe84a419f785948cd925f9ea0
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-08 19:12:17 +0600

    Water Requirement

---
+++ WATERREQ.cpp
@@ -0,0 +1,18 @@
+//Problem name: WATERREQ
+//https://www.codechef.com/problems/WATERREQ
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int T,N,i;
+
+    cin >> T;
+
+    for(i=0; i<T; i++){
+        cin >> N;
+        cout << N*2 << endl;
+    }
+
+    return 0;
+}
