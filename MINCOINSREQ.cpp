14203eeaaaa979b8c3635b67def9582987d85f15
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-09 10:08:07 +0600

    Minimum Coins

---
+++ MINCOINSREQ.cpp
@@ -0,0 +1,21 @@
+//Problem name: MINCOINSREQ
+//https://www.codechef.com/problems/MINCOINSREQ
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int T,X;
+
+    cin >> T;
+
+    while(T--){
+
+        cin >> X;
+
+        cout << X%10 << endl;
+        
+    }
+
+    return 0;
+}
