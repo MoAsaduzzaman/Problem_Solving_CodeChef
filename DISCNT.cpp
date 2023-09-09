27c5370249123917dd934883fb92e003973155db
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-10 00:18:21 +0600

    Discount

---
+++ DISCNT.cpp
@@ -0,0 +1,20 @@
+//Problem name: DISCNT
+//https://www.codechef.com/problems/DISCNT
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int T,x;
+
+    cin >> T;
+
+    while(T--){
+
+        cin >> x;
+        cout << 100-x << endl;
+
+    }
+
+    return 0;
+}
