d652a35d6d94d0c8c3c27aebf908966a1bc4490e
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-09 12:13:20 +0600

    Fill the Bucket

---
+++ FBC.cpp
@@ -0,0 +1,21 @@
+//Problem name: FBC
+//https://www.codechef.com/problems/FBC
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int T,K,X;
+
+    cin >> T;
+
+    while(T--){
+
+        cin >> K >> X;
+
+        cout << K-X << endl;
+        
+    }
+
+    return 0;
+}
