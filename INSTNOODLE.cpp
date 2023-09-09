0a5179b0b7aa89e8e3eac4c2e8f379f6bce570bf
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-10 00:08:31 +0600

    Chef and Instant Noodles

---
+++ INSTNOODLE.cpp
@@ -0,0 +1,14 @@
+//Problem name: INSTNOODLE
+//https://www.codechef.com/problems/INSTNOODLE
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int X,Y;
+
+    cin >> X >> Y;
+    cout << X*Y << endl;
+
+    return 0;
+}
