779d9f93a635d44682b88db594cea82c26c9a9cb
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-09 12:16:25 +0600

    Chef and Chocolates

---
+++ CCHOCOLATES.cpp
@@ -0,0 +1,21 @@
+//Problem name: CCHOCOLATES
+//https://www.codechef.com/problems/CCHOCOLATES
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int T,X,Y,Z;
+
+    cin >> T;
+
+    while(T--){
+
+        cin >> X >> Y >> Z;
+
+        cout << ((X*5)+(Y*10))/Z << endl;
+
+    }
+
+    return 0;
+}
