65ecf1d5308960fa0b70c1ae0c51fb90c402716e
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-12 00:32:37 +0600

    Janmansh and Coins

---
+++ JCOINS.cpp
@@ -0,0 +1,21 @@
+//Problem name: JCOINS
+//https://www.codechef.com/problems/JCOINS
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int T,X,Y;
+
+    cin >> T;
+
+    while(T--){
+
+        cin >> X >> Y;
+
+        cout << (X*10)+(Y*5) << endl;
+
+    }
+
+    return 0;
+}
