86d0d87fbea753a1e35fd98cb7bc58ab1d4dff01
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-12 00:33:46 +0600

    Shopping Change

---
+++ SHOPCHANGE.cpp
@@ -0,0 +1,20 @@
+//Problem name: SHOPCHANGE
+//https://www.codechef.com/problems/SHOPCHANGE
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int T,X;
+
+    cin >> T;
+
+    while(T--){
+
+        cin >> X;
+        cout << 100-X << endl;
+
+    }
+
+    return 0;
+}
