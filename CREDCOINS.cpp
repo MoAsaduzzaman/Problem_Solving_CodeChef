55e99d0ce2dc827ec6281a735385f4c42068ff14
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-12 00:31:04 +0600

    CRED Coins

---
+++ CREDCOINS.cpp
@@ -0,0 +1,21 @@
+//Problem name: CREDCOINS
+//https://www.codechef.com/problems/CREDCOINS
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int T,X,Y;
+
+    cin >> T;
+
+    while(T--){
+
+        cin >> X >> Y;
+
+        cout << (X*Y)/100 << endl;
+
+    }
+
+    return 0;
+}
