0b0795d990615d98b804630e4d478760c4fb319a
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-14 00:24:01 +0600

    AI Analysing Code

---
+++ AIANALYSE.cpp
@@ -0,0 +1,16 @@
+//Problem name: AIANALYSE
+//https://www.codechef.com/problems/AIANALYSE
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int C;
+    cin >> C;
+    if(C<=1000){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
