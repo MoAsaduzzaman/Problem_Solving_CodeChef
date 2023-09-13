24f7ea49479d5ee8792e8d601e0559a673aa34c7
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-14 00:31:22 +0600

    Credit score

---
+++ CREDSCORE.cpp
@@ -0,0 +1,16 @@
+//Problem name: CREDSCORE
+//https://www.codechef.com/problems/CREDSCORE
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int X;
+    cin >> X;
+    if(X>=750){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
