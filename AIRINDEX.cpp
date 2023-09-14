6fabc47f90bb3c504e83816265f51e89bd2d86ab
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-15 00:09:53 +0600

    Air Quality Index

---
+++ AIRINDEX.cpp
@@ -0,0 +1,16 @@
+//Problem name: AIRINDEX
+//https://www.codechef.com/problems/AIRINDEX
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int X;
+    cin >> X;
+    if(X<100){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
