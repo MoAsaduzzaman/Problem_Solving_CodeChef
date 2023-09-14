2d87aebe818041d0df1ddeee2d175a6330bff462
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-15 00:14:27 +0600

    Increase IQ

---
+++ INCRIQ.cpp
@@ -0,0 +1,16 @@
+//Problem name: INCRIQ
+//https://www.codechef.com/problems/INCRIQ
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int X;
+    cin >> X;
+    if((X+7)>170){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+    
+    return 0;
+}
