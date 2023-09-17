48c59132602c28670261e45c1f0df238c83b6a58
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-18 00:12:30 +0600

    Gymkhana Election IIIT-A

---
+++ CS2023_GYM.cpp
@@ -0,0 +1,16 @@
+//Problem name: CS2023_GYM 
+//https://www.codechef.com/problems/CS2023_GYM
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int T,N,M;
+    cin >> T;
+    while(T--){
+        cin >> N >> M;
+        cout << (M/2)+1 << endl;
+    }
+    
+    return 0;
+}
