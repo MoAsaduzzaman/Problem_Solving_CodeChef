181539c5a537035b3881a70c69458b35adcb8948
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-18 00:15:38 +0600

    Watching Movies at 2x

---
+++ MOVIE2X.cpp
@@ -0,0 +1,14 @@
+//Problem name: MOVIE2X
+//https://www.codechef.com/problems/MOVIE2X
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int X,Y;
+    cin >> X >> Y;
+    cout << (X-Y)+(Y/2) << endl;
+
+    return 0;
+}
+
