1ec2f2b66c253f85dd418c870794eec237ca5fb9
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-21 11:47:35 +0600

    Off By One

---
+++ OFFBY1.cpp
@@ -0,0 +1,11 @@
+//Problem name: OFFBY1
+//https://www.codechef.com/problems/OFFBY1
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int A,B;
+    cin >> A >> B;
+    cout << A+B << 1 << endl;
+    return 0;
+}
