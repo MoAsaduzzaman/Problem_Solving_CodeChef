7289276287b1d09d266c8d3cd0db74d645cd5e2b
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-09-28 00:02:29 +0600

    Sports Section

---
+++ NEWSPAPER.cpp
@@ -0,0 +1,16 @@
+//Problem name: NEWSPAPER
+//https://www.codechef.com/START102D/problems/NEWSPAPER
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int X;
+    cin >> X;
+
+    if(X>=8 && X<=10){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+    return 0;
+}
