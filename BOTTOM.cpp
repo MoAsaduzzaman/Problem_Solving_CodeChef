4fc285105433262c28d14f3bb7318c393c64237e
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-10-01 00:05:27 +0600

    Guess the bottom face

---
+++ BOTTOM.cpp
@@ -0,0 +1,15 @@
+//Problem name: BOTTOM
+//https://www.codechef.com/problems/BOTTOM
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T,X;
+    cin >> T;
+    while(T--){
+        cin >> X;
+        cout << 7-X << endl;
+    }
+    
+    return 0;
+}
