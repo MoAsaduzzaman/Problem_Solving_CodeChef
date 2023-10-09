d9a7e10c018d7d6b4eea522739f9e3ab7fcfd0dc
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-10-10 00:10:56 +0600

    Chef and the Hair Salon

---
+++ CHEFBARBER.cpp
@@ -0,0 +1,14 @@
+//Problem name: CHEFBARBER
+//https://www.codechef.com/problems/CHEFBARBER
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T,N,M;
+    cin >> T;
+    while(T--){
+        cin >> N >> M;
+        cout << N*M << endl;
+    }
+    return 0;
+}
