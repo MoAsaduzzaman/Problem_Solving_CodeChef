ed452e2b39108ed739bb875d102ebc3b9b5061d0
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-10-15 00:24:07 +0600

    Valid Triangles

---
+++ FLOW013.cpp
@@ -0,0 +1,20 @@
+//Problem name: FLOW013
+//https://www.codechef.com/problems/FLOW013
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int A,B,C;
+        cin >> A >> B >> C;
+
+        if((A+B+C)==180){
+        cout << "YES" << endl;
+        }else{
+        cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
