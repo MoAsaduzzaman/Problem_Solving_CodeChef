1fca034e15c2365aba54924a41d8acae4fc0bd35
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-10-17 00:14:43 +0600

    Helping Chef

---
+++ FLOW008.cpp
@@ -0,0 +1,20 @@
+//Problem name: FLOW008
+//https://www.codechef.com/problems/FLOW008
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int N;
+        cin >> N;
+
+        if(N<10){
+            cout << "Thanks for helping Chef!" << endl;
+        }else{
+            cout << -1 << endl;
+        }
+    }
+    return 0;
+}
