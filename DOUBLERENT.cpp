c3cd6600e180d591418c0535bc64462b3e3a4ea9
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-10-19 10:07:55 +0600

    Double Rent

---
+++ DOUBLERENT.cpp
@@ -0,0 +1,12 @@
+//Problem name: DOUBLERENT
+//https://www.codechef.com/START104D/problems/DOUBLERENT
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int X;
+	cin >> X;
+	cout << (2*X) << endl;
+	return 0;
+}
