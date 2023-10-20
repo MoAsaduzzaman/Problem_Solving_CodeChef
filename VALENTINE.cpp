5a892a545dc4c7751803433094d238aefbf2a6c5
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-10-20 09:47:52 +0600

    Valentine is Coming

---
+++ VALENTINE.cpp
@@ -0,0 +1,16 @@
+//Problem name: VALENTINE
+//https://www.codechef.com/problems/VALENTINE
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int X,Y;
+        cin >> X >> Y;
+        int div = X/Y;
+        cout << div << endl;
+    }
+    return 0;
+}
