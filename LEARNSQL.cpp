accb3fd85639ada7d650c4dc8698c37d5f7d7ba6
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-10-28 11:30:18 +0600

    Learning SQL

---
+++ LEARNSQL.cpp
@@ -0,0 +1,15 @@
+//Problem name: LEARNSQL
+//https://www.codechef.com/START90D/problems/LEARNSQL
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int R,C,E;
+    cin >> R >> C >> E;
+
+    int ans = (R+E)*C;
+
+    cout << ans << endl;
+
+    return 0;
+}
