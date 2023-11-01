5fd60b11fe35cb2df789cb6fa603425a8abe0488
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-02 00:12:06 +0600

    Fan Poll

---
+++ FIZZBUZZ2301.cpp
@@ -0,0 +1,18 @@
+//Problem name: FIZZBUZZ2301 
+//https://www.codechef.com/START106D/problems/FIZZBUZZ2301
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     
+     int A,B,C;
+     cin >> A >> B >> C;
+
+     if(A>B && A>C){
+        cout << "YES" << endl;
+     }else{
+        cout << "NO" << endl;
+     }
+    
+    return 0;
+}
