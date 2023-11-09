bd3560463a63688a5d6014a3b9a2496d1fbc1eba
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-09 09:44:48 +0600

    Clear Day

---
+++ CLEARDAY.cpp
@@ -0,0 +1,11 @@
+//Problem name: CLEARDAY 
+//https://www.codechef.com/START107D/problems/CLEARDAY
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int X,Y;
+    cin >> X >> Y;
+    cout << (7-(X+Y)) << endl;
+    return 0;
+}
