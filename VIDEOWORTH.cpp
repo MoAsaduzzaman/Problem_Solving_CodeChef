5f057fe835ee4b1ede8c3bc372d35344860ee83a
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-23 11:38:13 +0600

    Worth of a Video

---
+++ VIDEOWORTH.cpp
@@ -0,0 +1,14 @@
+//Problem name: VIDEOWORTH 
+//https://www.codechef.com/START109D/problems/VIDEOWORTH
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T,S;
+    cin >> T;
+    while(T--){
+        cin >> S;
+        cout << S*24*1000 << endl;
+    }
+    return 0;
+}
