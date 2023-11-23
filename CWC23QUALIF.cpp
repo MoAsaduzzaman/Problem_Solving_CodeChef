aeaca507eccba6d55f840192dbc41cf49fc49133
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-23 11:37:33 +0600

    Cricket World Cup Qualifiersolved

---
+++ CWC23QUALIF.cpp
@@ -0,0 +1,17 @@
+//Problem name: CWC23QUALIF 
+//https://www.codechef.com/START109D/problems/CWC23QUALIF
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int X;
+    cin >> X;
+
+    if(X>=12){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
