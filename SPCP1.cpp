975d07a6ac8b4b6ec4f1a7bbf199ab04308f90eb
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-11-30 00:00:09 +0600

    Water Park

---
+++ SPCP1.cpp
@@ -0,0 +1,18 @@
+//Problem name: SPCP1 
+//https://www.codechef.com/START110D/problems/SPCP1
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int W,H;
+    cin >> W >> H;
+
+    if(W>=60 && H<=130){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
