235c90ae5eb08ee6a23e92e6e1bed3b2f621e4b0
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-12-07 00:01:18 +0600

    404 Not Found

---
+++ ERROR404.cpp
@@ -0,0 +1,16 @@
+//Problem name: ERROR404 
+//https://www.codechef.com/START111D/problems/ERROR404
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int X;
+    cin >> X;
+
+    if(X==404){
+        cout << "NOT FOUND" << endl;
+    }else{
+        cout << "FOUND" << endl;
+    }
+    return 0;
+}
