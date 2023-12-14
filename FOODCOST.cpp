033db483ea54d3c5c423cd246c16a65c5511b606
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-12-15 01:06:43 +0600

    Good Food

---
+++ FOODCOST.cpp
@@ -0,0 +1,11 @@
+//Prbelm name: FOODCOST 
+//https://www.codechef.com/problems/FOODCOST
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int X,Y;
+    cin >> X >> Y;
+    cout << (X*6)+Y << endl;
+    return 0;
+}
