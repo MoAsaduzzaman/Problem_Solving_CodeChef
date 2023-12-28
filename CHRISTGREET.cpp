b735e65b3307f26362a8d147ec7a14057e7e8d2b
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-12-28 09:17:26 +0600

    Christmas Greetings

---
+++ CHRISTGREET.cpp
@@ -0,0 +1,17 @@
+// Problem name: CHRISTGREET 
+// https://www.codechef.com/START114D/problems/CHRISTGREET
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int X;
+    cin >> X;
+
+    if(X==25){
+        cout << "CHRISTMAS" << endl;
+    }else{
+        cout << "ORDINARY" << endl;
+    }
+
+    return 0;
+}
