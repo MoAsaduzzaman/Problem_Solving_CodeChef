daf4caabd65e6fd01ff67f90da0b41963a45e86e
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2024-01-04 09:54:21 +0600

    Codechef Round

---
+++ CODECHEF.cpp
@@ -0,0 +1,18 @@
+// Problem name: CODECHEF  
+// https://www.codechef.com/START115D/problems/CODECHEF
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+string asad(){
+    ll n;
+    cin >> n;
+    if(n==4) return "YES";
+    else return "NO";
+}
+
+int main(){
+    cout << asad() << endl;
+    return 0;
+}
