e337f0e44a083a1b9a85e8e90df97724c0bf38f3
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2024-01-12 19:23:09 +0600

    Minimum Wage

---
+++ MINWAGE.cpp
@@ -0,0 +1,17 @@
+// Problem name: MINWAGE
+// https://www.codechef.com/problems/MINWAGE
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int X;
+    cin >> X;
+
+    if(X>11){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+    
+    return 0;
+}
