4973ec6c50f45dd1b31c52629598026bcba4c609
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2024-01-17 21:59:55 +0600

    Phone Year

---
+++ PHONEYR.cpp
@@ -0,0 +1,11 @@
+// Problem name: PHONEYR 
+// https://www.codechef.com/START117D/problems/PHONEYR
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin >> s;
+    cout << "K" << s[2] << s[3] << endl;
+    return 0;
+}
