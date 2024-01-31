3110b8d12db9fceda7fb61ac0566a5eeafed595e
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2024-02-01 00:47:07 +0600

    Advitiya

---
+++ ADVITIYA1.cpp
@@ -0,0 +1,15 @@
+// Problem name: ADVITIYA1 
+// https://www.codechef.com/problems/ADVITIYA1
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    if(N==16 || N==17 || N==18){
+        cout << "ADVITIYA" << endl;
+    }else{
+        cout << "WAITING FOR ADVITIYA" << endl;
+    }
+    return 0;
+}
