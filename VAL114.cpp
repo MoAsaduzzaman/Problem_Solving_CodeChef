a828884d9e4b866bc08165ee8a8364e09872f9b2
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2024-02-09 20:01:12 +0600

    Valentines Contest

---
+++ VAL114.cpp
@@ -0,0 +1,15 @@
+// Problem name: VAL114 
+// https://www.codechef.com/problems/VAL114
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    if(N==121){
+        cout << "Likely" << endl;
+    }else{
+        cout << "Unlikely" << endl;
+    }
+    return 0;
+}
