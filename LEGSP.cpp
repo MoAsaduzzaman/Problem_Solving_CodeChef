6d771865b7fc68cc1d09e9b9be81a5bdb8fb56df
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2024-02-14 22:51:08 +0600

    Leg Space

---
+++ LEGSP.cpp
@@ -0,0 +1,15 @@
+// Problem name: LEGSP 
+// https://www.codechef.com/problems/LEGSP
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N,M;
+    cin >> N >> M;
+    if(N<M){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+    return 0;
+}
