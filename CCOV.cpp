8f1d714b9f9713dd5f59f5afe15d6adf310b4ef8
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2024-03-16 14:12:48 +0600

    Overspeeding

---
+++ CCOV.cpp
@@ -0,0 +1,17 @@
+// Problem name: CCOV 
+// https://www.codechef.com/problems/CCOV
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int S;
+    cin >> S;
+
+    if(S>40){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
